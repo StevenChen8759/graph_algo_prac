1a8c1e81abe9422c89d2279809a20d1b9cc869a7
Steven HH Chen <s335276@gmail.com>
2020-02-24 14:45:50 +0800

    Dynamic Allocation Array Test

--- graph_structure/graph_struct.c
+++ graph_structure/graph_struct.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*public class graph_adjlist{
+
+
+};*/
+
 int main(int argc, char *argv[]){
 
-    printf("test...\r\n");
-    return 0;
+	int *graph_matrix = (int*) calloc(5*5, sizeof(int));
+    int i, j;
+
+	for(i = 0 ; i < 5 ; i++){
+
+		for(j = 0 ; j < 5 ; j++)
+			printf("%d ", *(graph_matrix + 5*i + j));
+
+		printf("\n");
+
+	}
+
+	return 0;
+
 }
